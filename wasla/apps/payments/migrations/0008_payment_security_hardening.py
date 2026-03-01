ede7f7497bc053b0389eaa5ff74f573b4395cf35
mohammed0115 <mohemmedkamal0115498898@gmail.com>
2026-03-01 10:41:05 +0000

    database erro

--- wasla/apps/payments/migrations/0008_payment_security_hardening.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("payments", "0012_enterprise_payment_hardening"),
-    ]
-
-    operations = []
